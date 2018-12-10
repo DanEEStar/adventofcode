c9fe44dc6c94601959b77ead75ff3966e6d80666
Daniel Egger <daniel.egger@gmail.com>
2018-12-10 22:16:44 +0100

    Added setup for F#

---
+++ 2018/FSharpAdvent/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let text = IO.File.ReadAllText("day01.txt")
+    printfn("Hello")
+    printfn "%s" text
+    0 // return an integer exit code
